0cd0c1580d94ffef834b90700f66f04004f99d73
kartik <kartikb.5578@gmail.com>
2024-02-08 16:53:50 +0530

    form changes done

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -10,7 +10,7 @@ function Create({marketplace}) {
       title:"",
       description:"",
       owner:"",
-      price: null
+      price: 0
     });
 
     useEffect(()=>{
@@ -143,10 +143,10 @@ function Create({marketplace}) {
 
   <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
   <textarea onChange={handleChange}  name="description" id="description" rows="4" class="block p-2.5 w-full text-sm  mb-4 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-  <div class="mb-4">
+  {/* <div class="mb-4">
     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
     <input onChange={handleChange}  type="number" name='price' id="price" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder='0.001' required />
-  </div>
+  </div> */}
  <div className='text-center'>
 
 
